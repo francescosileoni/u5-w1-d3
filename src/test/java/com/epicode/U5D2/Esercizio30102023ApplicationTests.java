79857bc2914f748ef90a30c03706c27a94264970
francescosileoni <francesco.sile@gmail.com>
2024-04-04 17:15:22 +0200

    .

---
+++ src/test/java/com/epicode/U5D2/Esercizio30102023ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.epicode.U5D2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio30102023ApplicationTests {
+
+	@Test
+	void startApplication() {
+		System.out.println("Application started correctly");
+	}
+
+}
